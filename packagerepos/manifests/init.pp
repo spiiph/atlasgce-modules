34908988d3d8371a79e3e250d0393c9fb597ad52
Henrik Ohman <speeph@gmail.com>
2013-09-04 18:00:58 +0200

    Added an empty parameter list.

--- packagerepos/manifests/init.pp
+++ packagerepos/manifests/init.pp
@@ -1,7 +1,7 @@
 #
 # Required extra repositories.
 #
-class packagerepos {
+class packagerepos () {
   case $osfamily {
     'RedHat': { include packagerepos::yumrepos }
     'Debian': { alert('No extra package repos available for the Debian OS family.') }
