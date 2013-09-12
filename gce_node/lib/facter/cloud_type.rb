605310bd0443655a5d8155beec356dced0812130
Frank Berghaus <frank@uvic.ca>
2013-09-12 09:48:39 -0700

    moved cloud_type to /var/lib

--- gce_node/lib/facter/cloud_type.rb
+++ gce_node/lib/facter/cloud_type.rb
@@ -2,8 +2,8 @@ Facter.add("cloud_type") do
   has_weight 100
   setcode do
     # Check for existance of '/etc/distro-release' and check if it's CernVM
-    if File.exists?('/var/run/cloud_type')
-      Facter::Util::Resolution.exec("cat /var/run/cloud_type")
+    if File.exists?('/var/lib/cloud_type')
+      Facter::Util::Resolution.exec("cat /var/lib/cloud_type")
     end
   end
 end
